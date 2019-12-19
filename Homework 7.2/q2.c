05f7aedd67b85d16d3f8a95bac62f103e04312bd
Daniel Mizrahi <daniel.mizrahi@ruppin365.net>
2019-12-19 21:13:11 +0200

    working

--- Homework 7.2/q2.c
+++ Homework 7.2/q2.c
@@ -1,7 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
-#define  SIZE2 2
+#define  SIZE2 5
 
 void num_arrayinput(int array[], int size); // user enters a number and it will put it automatically into an array
 void is_mirrored_array(const int array1[], int size1); // check if 2 arrays are mirrored
@@ -10,12 +10,10 @@ void main()
 {
 	int arr1[SIZE2] = { 0 };
 	printf("Enter 6 numbers: \n");
-	
 	num_arrayinput(arr1, SIZE2);
-	
-	printf("Calculating... \n");
+	printf("----------\nCalculating\n----------\n");
 	is_mirrored_array(arr1, SIZE2);
-	printf("Done... \n");
+
 }
 
 void num_arrayinput(int array[], int size)
@@ -34,33 +32,35 @@ void num_arrayinput(int array[], int size)
 void is_mirrored_array(const int array1[], int size1)
 {
 	//call function to enter numbers into arrays
-	//num_arrayinput(array1, size1);
-	int refresher1, oldnum1, newnum1 = 0, value = 0;//init 0, 1 true, 2 false
+	int oldnum1,value = 0; //init 0, 1 true, 2 false
 	
-	for (int u = 0, j = size1; j < u; ++u, --j) // U:A0->An && J:An->U
+	for (int u = 0, j = size1-1; j > u; ++u, --j) // U:A0->An && J:An->U
 	{
-		if ((array1[u] < 0) || (array1[j] < 0)) // stop program because of invalid input
+		if ((array1[u] < 0) || (array1[j] < 0) || (size1%2!=0)) // stop program because of invalid input
 		{
 			printf("Error, invalid input!\n");
 			break;
 		}
 
 		oldnum1 = array1[u];
+		int newnum1 = 0;
 		
-			while(oldnum1>0) // reverse numbers
+			while(oldnum1!=0) // reverse numbers
 			{
-				refresher1 = oldnum1 % 10;
-				newnum1 = (newnum1+refresher1) * 10;
+				newnum1 = newnum1 * 10;
+				newnum1 = newnum1 + oldnum1%10;
 				oldnum1 = oldnum1 / 10;
 		
 			}
-			value = (array1[j] == newnum1) ? value == 1 : value == 2;
+		
+			value = (array1[j] == newnum1) ? 1 : 2;
 		if (value==2)
 		{
 			printf("The array is not a mirrored array!\n");
 			break;
 		}
 	}
+	
 	if (value == 1)
 	{
 		printf("The array is a mirrored array!\n");
