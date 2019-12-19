788a931b86dd00f9043f9fcd5edb6a05d0acd28d
Daniel Mizrahi <daniel.mizrahi@ruppin365.net>
2019-12-19 17:30:16 +0200

    update 2

--- Homework 7.1/q1.c
+++ Homework 7.1/q1.c
@@ -1,8 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+
+#define  SIZE 5
+
+int isnum_reverse(int array1[], int size1, int array2[], int size2);
+void num_arrayinput(int array[], int size);
 
 void main()
 {
+	int arr1[SIZE] = { 0 };
+	int arr2[SIZE] = { 0 };
+	
+	printf("Enter 2 numbers: \n");
+	num_arrayinput(arr1, SIZE);
 
-	hi
+}
 
+int isnum_reverse(int array1[], int size1, int array2[], int size2)
+{
+	int value = 0;
+	for (int i = 0, j = size2; i < size1, j>0;i++, j++)
+	{
+		if (array1[i] != array2[j])
+		{
+			value=1;
+			break;
+		}
+		if (array1[i] == array2[j])
+		{
+			value = 0;
+		}
+		
+	}
+
+	return value;
+}
+
+void num_arrayinput(int array[], int size)
+{
+	int temp_num, solo_num;
+	printf("Enter a number: \n");
+	scanf("%d", &temp_num);
+	for (int i = size; i > 0; ++i)
+	{
+		solo_num = temp_num % 10;
+		array[i] = solo_num;
+		temp_num /= 10;
+	}
+	
 }
