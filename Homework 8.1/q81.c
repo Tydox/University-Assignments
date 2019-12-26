68f913335d2fd51c00ddf9df3ab9d0e25d446a91
Tydox <40888907+Tydox@users.noreply.github.com>
2019-12-26 18:31:40 +0200

    homework 8 update 1 & update to vsproj from uni lab

--- Homework 8.1/q81.c
+++ Homework 8.1/q81.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
-
-void myStrcpy(int* strA, int* strB);
+#define SIZE 11
+void myStrcpy(char strA[], char strB[]);
 
 void main()
 {
-	int *str1;
-	int* str2;
+	char str1[SIZE] = { '\0' };
+	char str2[SIZE] = { '\0' };
+	printf("Enter a tav: \n");
+	scanf_s(" %s", str1, SIZE-1);
+	printf("Enter a tav: \n");
+	scanf_s(" %s", str2, SIZE-1);
+	printf("Copying: \n");
+	myStrcpy(str1, str2);
+	printf("A: %s\nB: %s", str1, str2);
+
 }
 
-void myStrcpy(int* strA, int* strB)
+void myStrcpy(char strA[], char strB[])
 {
-	int temp;
-	temp = strA;
-	*strA = strB;
-	*strB = temp;
+	int i;
+	for(i=0 ; strA[i] != '\0';++i)
+	{
+		strB[i] = strA[i];
+		
+	}
+	strB[i] = '\0';
 }
