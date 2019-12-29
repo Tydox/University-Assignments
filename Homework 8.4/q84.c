38b2d94fe89d5898f99b68fa45cadad255fd5d64
Tydox <40888907+Tydox@users.noreply.github.com>
2019-12-29 17:37:03 +0200

    q8.4 done

---
+++ Homework 8.4/q84.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#define SIZE 501
+
+void password_encrypt(char passwordstr[], int idx);
+
+void main()
+{
+	char pstr[SIZE] = { '0' };
+	int indexnum=0;
+	printf("Enter your text [%d]: \n", SIZE - 1);
+	scanf_s(" %[^\n]s", pstr, SIZE - 1);
+	printf("Enter your index to encrypt: \n");
+	scanf_s("%d", &indexnum);
+	password_encrypt(pstr, indexnum);
+	printf("%s", pstr);
+}
+
+void password_encrypt(char passwordstr[], int idx)
+{
+	char tempstr[501] = { '\0' };
+	int i = idx;
+	int j = 0;
+	for (j; passwordstr[i] != '\0'; ++i, ++j) // copy text after cut
+	{
+		tempstr[j] = passwordstr[i];
+	}
+	for (int i = 0; i < idx; ++i,++j) // copy text before the cut
+	{
+		tempstr[j] = passwordstr[i];
+	}
+
+	
+	for (int i = 0; tempstr[i] != '\0'; ++i)
+	{
+		passwordstr[i] = tempstr[i];
+		
+	}
+	passwordstr[j + 1] = '\0';
+}
