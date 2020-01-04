5e8dc1ab1940134c5bc3e4c3c2e76b1c83951d85
Tydox <40888907+Tydox@users.noreply.github.com>
2020-01-04 22:54:06 +0200

    q91 done

---
+++ Homework 9.1/q91.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#define SIZE 40
+void removeDigits(char* str);
+
+void main()
+{
+	char str[SIZE] = { "Today I had 3 apples and 2 bananas1!" };
+	removeDigits(str);
+	printf("%s", str);
+}
+
+void removeDigits(char* str)
+{
+	int i = 0;
+	
+	while (str[i] != '\0')
+	{
+	
+		if ((((*(str + i) >= '0') && (*(str + i) <= '9'))))
+		{
+			int j = i;
+			while(*(str + j) != '\0')
+			{
+				*(str + j) = *(str + j + 1);
+				++j;
+			}
+		}
+		++i;
+		
+	}
+}
